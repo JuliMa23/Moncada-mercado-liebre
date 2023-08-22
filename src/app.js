10b919b7c0c8b850b4cae35890161459f0c72ab2
JuliMa23 <julianamoncada41@gmail.com>
2023-08-21 20:12:59 -0500

    Render

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const path= require ("path")
 const app= express()
 app.use(express.static(path.join(__dirname,'../public')))
 
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.listen( PORT, ()=> {
     console.log(`El servidor se ha iniciado correctamente en el puerto ${PORT}`)
